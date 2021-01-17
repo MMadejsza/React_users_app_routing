c280ba99c81a31406f9bca675ffe7810b320c329
Maciej <maciejmad16@gmail.com>
2021-01-17 11:13:53 +0000

    proper package installed [...]router-dom

--- 17_01_2021-users_excersise/src/App.js
+++ 17_01_2021-users_excersise/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import { BrowserRouter as Router, Route, Link } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './Components/Home'
 import './Components/UsersList'
 
@@ -9,13 +9,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <Link to="/">
+            <p>Home</p>
+          </Link>
+          <Link to="/user-list">
+            <p>User List</p>
+          </Link>
+
+        </Router>
       </div>
     );
   }
